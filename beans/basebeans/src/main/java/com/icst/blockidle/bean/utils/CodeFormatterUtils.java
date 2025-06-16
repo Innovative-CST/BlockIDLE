93065c44c174e9af45bdd99f5776c0a6b624f4d7
DevVigilante <96680248+DevVigilante@users.noreply.github.com>
2025-06-16 16:49:57 +0530

    doc: add documentation for CodeFormatterUtils

--- beans/basebeans/src/main/java/com/icst/blockidle/bean/utils/CodeFormatterUtils.java
+++ beans/basebeans/src/main/java/com/icst/blockidle/bean/utils/CodeFormatterUtils.java
@@ -17,36 +17,69 @@
 
 package com.icst.blockidle.bean.utils;
 
+/**
+ * Utility class providing methods for formatting and analyzing code strings, such as generating
+ * syntax keys, applying indentation, and calculating indentation levels.
+ *
+ * <p>This class is not intended to be instantiated or extended.
+ */
 public final class CodeFormatterUtils {
 
-	public static String getKeySyntaxString(String key) {
-		return new String("<CodeKey : ").concat(key).concat(">");
-	}
+    /**
+     * Formats the given key as a custom syntax string in the format {@code <CodeKey : key>} which
+     * is used by Block IDLE application.
+     *
+     * @param key the key to be wrapped in the syntax format
+     * @return a formatted string containing the given key
+     */
+    public static String getKeySyntaxString(String key) {
+        return new String("<CodeKey : ").concat(key).concat(">");
+    }
 
-	public static String addIntendation(String code, int intendation) {
-		StringBuilder intendedCode = new StringBuilder();
-		String[] lines = code.split("\n");
-		for (int i = 0; i < lines.length; ++i) {
-			String line = lines[i];
-			if (i != 0) {
-				intendedCode.append("\t".repeat(intendation));
-			}
-			intendedCode.append(line);
-			intendedCode.append("\n");
-		}
-		return intendedCode.toString();
-	}
+    /**
+     * Adds tab-based indentation to each line of the given code string.
+     *
+     * <p>The first line is kept as-is, and all subsequent lines are prefixed with the specified
+     * number of tab characters. Read {@link #getIntendation} to know why intendation is nog applied
+     * to first line.
+     *
+     * @param code the multi-line code string to format
+     * @param intendation the number of tab characters to prepend to each line (except the first)
+     * @return the indented code string
+     */
+    public static String addIntendation(String code, int intendation) {
+        StringBuilder intendedCode = new StringBuilder();
+        String[] lines = code.split("\n");
+        for (int i = 0; i < lines.length; ++i) {
+            String line = lines[i];
+            if (i != 0) {
+                intendedCode.append("\t".repeat(intendation));
+            }
+            intendedCode.append(line);
+            intendedCode.append("\n");
+        }
+        return intendedCode.toString();
+    }
 
-	public static int getIntendation(String codeSyntax, String item) {
+    /**
+     * Calculates the indentation (leading space count) of the first line in the given code that
+     * contains the specified item.
+     *
+     * @param codeSyntax the full code block to search through
+     * @param item the string to locate in the code
+     * @return the number of leading spaces (indentation level) before the item, or {@code 0} if the
+     *     item is not found
+     */
+    public static int getIntendation(String codeSyntax, String item) {
 
-		String[] codeLines = codeSyntax.split("\n");
-		for (String line : codeLines) {
-			if (line.contains(item)) {
-				int spaceCount = line.indexOf(item);
-				return spaceCount;
-			}
-		}
+        String[] codeLines = codeSyntax.split("\n");
+        for (String line : codeLines) {
+            if (line.contains(item)) {
+                int spaceCount = line.indexOf(item);
+                return spaceCount;
+            }
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 }
